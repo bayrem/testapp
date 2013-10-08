c4ebc2601d58178b70b421e7d0d4e8f026c057b5
Bayrem <bayrem_jridi@hotmail.com>
2013-10-08 16:35:52 +0200

    2nd commit

---
+++ testapp/ProfileEditPage.xaml.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace testapp
+{
+    /// <summary>
+    /// Interaction logic for ProfileEditPage.xaml
+    /// </summary>
+    public partial class ProfileEditPage : Page
+    {
+        public ProfileEditPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_Abort(object sender, RoutedEventArgs e)
+        {
+            var mainPage = new MainPage();
+            this.NavigationService.Navigate(mainPage);
+        }
+
+        private void Button_Click_Send(object sender, RoutedEventArgs e)
+        {
+            var mainPage = new MainPage();
+            this.NavigationService.Navigate(mainPage);
+        }
+
+       
+    }
+}
